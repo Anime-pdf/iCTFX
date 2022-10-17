4e0cb7897200dfad2472bfca61b3f560981907cd
Paul <paul.turkovskiy@gmail.com>
2022-10-17 23:08:48 +0300

    better structure

--- src/engine/server/databases/connection.cpp
+++ src/engine/server/databases/connection.cpp
@@ -6,16 +6,16 @@ void IDbConnection::FormatCreateUsers(char *aBuf, unsigned int BufferSize)
 {
 	str_format(aBuf, BufferSize,
 		"CREATE TABLE IF NOT EXISTS `stats` ("
-			"`name` nvarchar(255) PRIMARY KEY,"
-			"`kills` int,"
-			"`deaths` int,"
-			"`touches` int,"
-			"`captures` int,"
-			"`fastest_capture` int,"
-			"`suicides` int,"
-			"`shots` int,"
-			"`wallshots` int,"
-			"`wallshot_kills` int"
+			"`name` nvarchar(255) PRIMARY KEY not null,"
+			"`kills` int not null default 0 check(`kills` >= 0),"
+			"`deaths` int not null default 0 check(`deaths` >= 0),"
+			"`touches` int not null default 0 check(`touches` >= 0),"
+			"`captures` int not null default 0 check(`captures` >= 0),"
+			"`fastest_capture` int not null default 0 check(`fastest_capture` >= 0),"
+			"`suicides` int not null default 0 check(`suicides` >= 0),"
+			"`shots` int not null default 0 check(`shots` >= 0),"
+			"`wallshots` int not null default 0 check(`wallshots` >= 0),"
+			"`wallshot_kills` int not null default 0 check(`wallshot_kills` >= 0)"
 		");"
 		);
 }
@@ -24,9 +24,9 @@ void IDbConnection::FormatCreateServer(char *aBuf, unsigned int BufferSize)
 {
 	str_format(aBuf, BufferSize,
 		"CREATE TABLE IF NOT EXISTS `stats_server` ("
-			"`server_name` nvarchar(255) PRIMARY KEY,"
-			"`red_score` int,"
-			"`blue_score` int"
+			"`server_name` nvarchar(255) PRIMARY KEY not null,"
+			"`red_score` int null default 0 check(`red_score` >= 0),"
+			"`blue_score` int null default 0 check(`blue_score` >= 0)"
 		");"
 		);
 }
